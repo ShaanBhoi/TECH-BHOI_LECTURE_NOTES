84b18fdb45533ea728639c56895bf8efa6510396
Shaan Bhoi <TechBhoi@gmail.com>
2020-07-06 06:29:42 +0000

    added lecture 4 Data Types and the PDF

---
+++ main.py
@@ -0,0 +1,84 @@
+## Data Types
+
+## how to check the type of the value stored in a variable
+## type(Variable_name)
+myName = "John Doe"
+print(myName,"is of type",type(myName))
+############################################################################
+##What are the main types?
+
+## numeric data types
+## we have integers and floats
+
+## integers are whole numbers
+myAge = 10
+type(myAge)
+##floats have decimal places
+myPi = 3.14
+type(myPi)
+
+#Quiz Time
+'''
+Set an integer as the value of x
+
+x = ________
+type(x)
+
+a) 3.4
+b) 5.67
+c) 832749834
+
+Set a float as the value of y
+
+x = ________
+type(y)
+
+a) 3
+b) 5.67
+c) 832749834
+
+3. What is it called when a variable value is a number?
+
+a) Integer type
+b) Float type
+c) Numeric type
+
+'''
+
+##################################
+
+##we have two other data tyoes
+## Boolean 
+
+imHappy = True
+type(imHappy)
+
+imSad = False
+type(imSad)
+
+## strings
+
+mySentence = "i like to ride my bike"
+type(mySentence)
+
+##Quiz Time
+'''
+1. What is missing to set a string as a value of the friend variable?
+
+friend = "Usain Bolt ___
+print(friend)
+a) "
+b) ;
+c) '
+
+2. What is the correct string?
+
+a) movie = 'Inception"
+b) movie = "The Great Gatsby
+c) movie = 'Napolean Dynamite'
+
+'''
+
+
+############################################################################
+
