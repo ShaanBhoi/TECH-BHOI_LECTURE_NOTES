14079149bdfa44a966ed20fc0a29a2e12c131cf8
Shaan Bhoi <TechBhoi@gmail.com>
2020-07-07 15:49:22 +0000

    lecture 5 completed

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
-## pop quiz
+#myName = input("What is your name : ")
+## variable_name = value
+## value is : input("What is your name : ")
+#print (myName)
+
 '''
-1. How to show output (in the terminal) from a program (written in the text editor)?
-
-a) Print() 
-b) output()
-c) print()
-
-2. Which variable stores a string value
-
-a) fruit = 'strawberry'
-b) weight = 678
-c) is_winter = false
-
-3. What is the expected output of the following program?
-
-myNum = 5
-myNum = myNum + myNum
-print(myNum)
-
-4.What would be the output of the following program?
-
-cop = 'Paul Blart'
-Cop = "Paul Blart"
-cop = "Jason Statham"
-print(cop)
-
-
-5. If it is over 30 degrees today, then it is too hot. What is the boolean value to complete the variable 
-
-tooHot = _______
-
-6. Which of the following is not a numeric data type variable?
-
-a) myAge = 15
-b) tooHot = False
-c) myTime = 11.30
+### your name, wher u are living, two movies u like, and ur favourite baskteball player
+## using one line or multiple, if using multiple call the function each line
+print("MY INFROMATION: ")
+print("My name is Rohan","I live in Mississauga")
+print("MY FAVOURITES: ")
+print("my favourite movie is Inception an the runner up is the Matrix","and my favourite basketball player is Lebron James")
 '''
+
+myName = input("What is your name : ")
+myCity = input ("where do you live? : ")
+myActivity= input("What is your favourite hobby : ")
+myAge = int(input("What is your age : "))
+
+
+#myInfo= "my name is Rishi, I live near New York City. I like to play Xbox."
+print ("\nMy Name is" , myName,"\nI live in" ,myCity,"\nI like to" ,myActivity,"\n I am",myAge, "years old" )
+
+myNumber = float(input("What is your fave number : "))
+print(myNumber*2)
