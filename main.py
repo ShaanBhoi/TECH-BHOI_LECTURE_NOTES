427c26c1950544b47e711007b5fb91f979a37346
Shaan Bhoi <TechBhoi@gmail.com>
2020-07-17 07:59:34 +0000

    tutorial 3

---
+++ main.py
@@ -0,0 +1,29 @@
+################
+## question 1 ##
+################
+## create an if statement that checks a variable is exactly equal to the value stored in the variable, create an output for this
+
+################
+## question 2 ##
+################
+## expand q1 to have an else statement
+
+################
+## question 3 ##
+################
+## create an if condition that compares two objects (not using exactly equals), have an output, create an elif statement with two different objects (not using ==), have an output, create an else statement with an output.
+
+
+################
+## question 4 ##
+################
+## create a letter grade calculator, by using multiple elif statements with multiple conditions and printing appropriate information. grade is given by input A:[80,inf),B:[70,80),C:[60,70),D:[50,60),F:[-inf,50)
+
+
+################
+## question 5 ##
+################
+## expand q3 to have +/- for increments of 5 
+## implement if [40,50) u have an E, output that a student has opportunity to take a supplemental exam
+## implement F grade for (1,40)
+## implement ABS for absent [0,1]
