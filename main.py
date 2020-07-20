17c834aee192624768e27db79134ddb482bf4564
Shaan Bhoi <TechBhoi@gmail.com>
2020-07-20 09:14:20 +0000

    made lec 10 (lists)+ notes + pdf

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
-################
-## question 1 ##
-################
-## create an if statement that checks a variable is exactly equal to the value stored in the variable, create an output for this
-
-################
-## question 2 ##
-################
-## expand q1 to have an else statement
-
-################
-## question 3 ##
-################
-## create an if condition that compares two objects (not using exactly equals), have an output, create an elif statement with two different objects (not using ==), have an output, create an else statement with an output.
+Now that we know all of our 4 main data types like the back of our hand...
 
 
-################
-## question 4 ##
-################
-## create a letter grade calculator, by using multiple elif statements with multiple conditions and printing appropriate information. grade is given by input A:[80,inf),B:[70,80),C:[60,70),D:[50,60),F:[-inf,50)
+We are going to introduce a new one, called "List".
+
+This is like a special data type. 
+
+We can use a list to store multiple different data types (or all the same or a mix however you wanna use it)
+
+Can you give some examples of why lists are useful? Think of some harder ones, grocery lists and names of friends is too easy...
+
+Using the same way that we declare variables to store other data types, we will use the same format to store a list as the value for the variable.
+What is unique about this is that we can then in turn store values of data in the list. 
+
+Sometimes you will hear people call "lists" an "array" and vice versa, they refer to the same thing.
+
+## How to declare a variable
+variableName = value 
+
+## how to declare a list
+
+listName = [val1,val2,...,valN]
+
+##Example: We want to create a list (with an appropriate name) to store all the names of our family members
+
+myFamily = ["sister","brother","dog","mother","father"]
+
+When declaring a list, we need 2 things:
+1. square brackets to contain the things we want to put in the list <- we will refer to these things as elements
+2. commas to seperate each element in the array 
+
+___________
+
+Quiz:
+declare a variable with the name shapes, store inside of it the strings of square,rectangel,circle 
+
+shapes = ["square", "rectangle", "circle"]
+
+___________
+Quiz :
+
+What are the two most important parts in creating a list?
+
+a) the equals sign and parenthses
+b) square parenthses and quotes
+c) square parenthses and commas
+d) open square parenthses and a close square parenthses 
 
 
-################
-## question 5 ##
-################
-## expand q3 to have +/- for increments of 5 
-## implement if [40,50) u have an E, output that a student has opportunity to take a supplemental exam
-## implement F grade for (1,40)
-## implement ABS for absent [0,1]
+________
+Can you fix the code ?
+
+colours = ("strawberry red' , peach pink : "sundown orange"],
+
+___________
+
+Use a list to store more than one value in a variable.
