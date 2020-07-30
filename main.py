6440566b41e36aec6e470787cc520014f6b81568
Shaan Bhoi <TechBhoi@gmail.com>
2020-07-30 02:18:30 +0000

    added coderun for the 00 lecture, it outputs textbook text to the terminal

---
+++ main.py
@@ -0,0 +1,30 @@
+## Lecture_00
+
+def summaryOfSections00():
+	print("\n----------\nLECTURE 00\n----------\n")
+	
+	
+	##print("--PYTHON--")
+	##print("--PROBLEM SOLVING--")
+
+def sectionPython():
+	print("--PYTHON--")
+	text = "Python is one of the best programming languages to learn, it is ranked as the top most commnly used programming language.\nIt is very useful to learn for beginner coders, because the language itself has a syntax and readability almost like in English.\nIt is a popular programming language for machine learning and deep learning applications.\nThis language is quick to implement ideas with simple code.\n"
+	print(text)
+	return text
+
+def sectionPS():
+
+	print("--PROBLEM SOLVING--")
+	text = "The goal of this course is to teach you how to think like a computer scientist, and understand the foundations of how to code (in many popular languages).\nProblem solving is the ability to formulate problems, think creatively about solutions, and express a solution accurately and efficiently.\nAs it turns out, the process of learning how to program is an excellent opportunity to practice problem solving skills.\n"
+	print(text)
+	return text
+
+def runLec00():
+	summaryOfSections00()
+	sectionPython()
+	sectionPS()
+
+runLec00()
+
+
